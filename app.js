7d7db06c0dc5479181f838cbe3cf329ae79f8571
se040054 <peter8822967@gmail.com>
2023-07-17 21:39:24 +0800

    view : basic template , css

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
 
+app.engine('.hbs', engine({extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views', './views')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('password generator !')
+  res.render('index')
+  
 })
 
 app.listen(port, () => {
-  console.log(`Click :http://localhost:3000 }`)
+  console.log(`Click :http://localhost:3000`)
 })
