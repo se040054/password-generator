878fc4d363c18f36521481b5d465e55e837f62c3
se040054 <peter8822967@gmail.com>
2023-07-18 06:59:22 +0800

    feat : get method "POST" from form , and use data add : index.js function

--- app.js
+++ app.js
@@ -7,12 +7,29 @@ app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false })) 
+
+const js=require('./public/javascript/index.js')
+
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.redirect('/password-generator')
   
+})
+
+app.get('/password-generator',(req,res)=>{
+  res.render('index')
+})
+
+app.post('/form',(req,res)=>{
+  
+  const data=req.body
+  console.log(data.extent)
+  js.generate(data)
+  res.render('index')
 })
 
 app.listen(port, () => {
   console.log(`Click :http://localhost:3000`)
+  
 })
---
+++ public/javascript/index.js
@@ -0,0 +1,6 @@
+module.exports.generate =function (data){
+  console.log("from generate")
+  console.log(data.extent)
+}
+
+
