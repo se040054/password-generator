88012c5f3e06a672c6a43028857c0cc9b89bc522
se040054 <peter8822967@gmail.com>
2023-07-18 08:06:58 +0800

    feat : generate password and render

--- app.js
+++ app.js
@@ -2,19 +2,15 @@ const express = require('express')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
-
 app.engine('.hbs', engine({extname: '.hbs'}))
 app.set('view engine', '.hbs')
 app.set('views', './views')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: false })) 
-
 const js=require('./public/javascript/index.js')
-
 
 app.get('/', (req, res) => {
   res.redirect('/password-generator')
-  
 })
 
 app.get('/password-generator',(req,res)=>{
@@ -22,14 +18,14 @@ app.get('/password-generator',(req,res)=>{
 })
 
 app.post('/form',(req,res)=>{
-  
   const data=req.body
-  console.log(data.extent)
-  js.generate(data)
-  res.render('index')
+  const password = js.generate(data)
+  // console.log(password)
+  res.render('index',{password})
 })
 
 app.listen(port, () => {
+
   console.log(`Click :http://localhost:3000`)
-  
+
 })
