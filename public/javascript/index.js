40b3e72a74181f37c90bfa8d941036ed924cd8a8
se040054 <peter8822967@gmail.com>
2023-07-18 08:06:22 +0800

    feat : generate password and render

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,6 +1,25 @@
 module.exports.generate =function (data){
-  console.log("from generate")
-  console.log(data.extent)
+  const type1="abcdefghijklmnopqrstuvwxyz";
+  const type2="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const type3="0123456789";
+  const type4="!#$%&()*+,-./:;<=>?@[]^_{|}~"; 
+  let AllChars="";
+  const length=Number(data.extent)
+  let password=''
+  if (data.lowerCase==='on'){
+    AllChars+=type1
+  }
+  if (data.upperCase==='on'){
+    AllChars+=type2
+  }
+  if (data.number==='on'){
+    AllChars+=type3
+  }
+  if (data.symbol==='on'){
+    AllChars+=type4
+  }
+  while (password.length<length){
+    password += AllChars[Math.floor(Math.random()*AllChars.length)]
+  }
+  return password ;
 }
-
-
