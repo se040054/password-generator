30950f7f9dbe628fc7a598e483038c2e79376f80
se040054 <peter8822967@gmail.com>
2023-07-20 03:35:36 +0800

    add : render popover , modify CSS

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -49,5 +49,4 @@ function excludeChars(string){ //放在外面會讀不到data
 function onCopyClicked(){
   event.preventDefault(); //雖然這裡標註錯誤 但其實可用
   navigator.clipboard.writeText(password) //這邊password是從從main.hbs模板語法let password = "{{password}}"傳遞 不是上面那塊
-  alert(`Copy : ${password}`)
 }
